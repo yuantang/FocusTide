f7164f1aef66d61b589791ef92f446a25ca638aa
tangyuan <tangyuan@17mingxiang.com>
2025-04-27 19:50:21 +0800

    添加 Microsoft Clarity 分析服务

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -84,7 +84,23 @@ export default defineNuxtConfig({
       ],
       link: [
         { rel: 'icon', href: '/favicon.svg' }
-      ]
+      ],
+      script: [
+        {
+          hid: 'clarity-script',
+          innerHTML: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "raojqv0iyn");
+          `,
+          type: 'text/javascript'
+        }
+      ],
+      __dangerouslyDisableSanitizersByTagID: {
+        'clarity-script': ['innerHTML']
+      }
     }
   },
 
